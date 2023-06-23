6d14e4f0148bf2cc607df4f9545f993ad0926011
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-06-23 19:17:25 +0500

    6-masala

--- if06.py
+++ if06.py
@@ -6,4 +6,27 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    b = n//10000
+    c = (n//1000)%10
+    d = (n//100)%10
+    e = (n//10)%10
+    f = n%10
+    mx = max(b,c,d,e,f)
+    md = 0
+    if mx==b:
+        md+=1
+    if mx==c:
+        md+=2
+    if mx==d:
+        md+=3
+    if mx==e:
+        md+=4
+    if mx==f:
+        md+=5
+    return md
+n = 12345
+print(main(n))
+n=32897
+print(main(n))
+n=46935
+print(main(n))
